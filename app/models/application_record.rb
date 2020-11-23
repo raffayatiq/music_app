345b80d88ea13c32faa3b4c13e988abbcc093c8f
Raffay Atiq <raffayatiq2341@gmail.com>
2020-11-23 16:28:02 +0500

    Added country code getter method to add country codes to youtube api query.:

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,6 +3,9 @@ class ApplicationRecord < ActiveRecord::Base
   YOUTUBE_API_KEY = 'AIzaSyAzwGhEYWBJKUbTHiBUOh40NoJRa61BMBo'.freeze
 
   def query_youtube_api(search_query)
+  	country_code = get_country_code
+  	search_query += "&regionCode=#{country_code}" if country_code
+
   	retries = 0
 	begin		
 		uri = URI("https://www.googleapis.com/youtube/v3/search?key=#{YOUTUBE_API_KEY}&q=#{search_query}&part=snippet,id&order=relevance&maxResults=1")
@@ -22,4 +25,11 @@ class ApplicationRecord < ActiveRecord::Base
 	return nil if search_results.empty?
 	search_results.first['id']
   end
+
+	def get_country_code
+		public_ip_address = open('http://whatismyip.akamai.com').read
+
+		response = Net::HTTP.get( URI.parse( "https://www.iplocate.io/api/lookup/#{public_ip_address}" ) )
+		JSON.parse(response)["country_code"]
+	end
 end
